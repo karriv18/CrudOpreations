2e204fef5f0ea6e07caed0922e2fb2f7870da96e
karriv18 <krlclarence@gmail.com>
2025-03-27 22:27:05 +0800

    Add project files.

---
+++ CrudOpreations/Configurations/CrudSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace CrudOpreations.Configurations;
+
+public class CrudSettings
+{
+     public required string ModuleKey { get; set; }
+     
+     public required string UploadDirectory { get; set; }
+     public required string ConnectionString { get; set; }
+     public required string ModuleCode { get; set; }
+
+
+}
---
+++ CrudOpreations/Database/DbConnectionFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using CrudOpreations.Configurations;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace CrudOpreations.Database;
+
+public interface IDbConnectionFactory
+{
+    Task<IDbConnection> CreateConnectionAsync(CancellationToken cancellationToken = default);
+}   
+public class DbConnectionFactory : IDbConnectionFactory
+{
+    private readonly string _connectionString;
+
+    public DbConnectionFactory(IOptionsMonitor<CrudSettings> configurations) => 
+        _connectionString = configurations.CurrentValue.ConnectionString;
+
+    public async Task<IDbConnection> CreateConnectionAsync(CancellationToken cancellationToken = default)
+    {
+        var connection = new SqlConnection(_connectionString);
+        await connection.OpenAsync(cancellationToken);
+
+        return connection;
+    }
+}
